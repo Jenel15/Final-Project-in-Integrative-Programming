445e131185ea5757b72a2d99e6693b98bed9f22f
Jenel15 <125538150+Jenel15@users.noreply.github.com>
2023-06-02 03:51:22 +0800

    Add files via upload

---
+++ parts/footer.php
@@ -0,0 +1,7 @@
+<footer class="text-center text-capitalize bg-dark text-white p-3">
+        <h4>Final Project In Integrative Programming</h4>
+        <h5>Design & Developed By Jenel Ricafrente & Cherylyn Sales</h5>
+        <h6>Follow Us On Instagram: @iamjenpr & @chchei_</h6>
+    </footer>
+</body>
+</html>
